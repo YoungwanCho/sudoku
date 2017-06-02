da962883a08e3f269993bc99f9afa3759ee86301
developer119 <developer119korea@gmail.com>
2017-06-02 17:26:44 +0900

    add Classs

---
+++ Assets/scripts/DefineData.cs
@@ -0,0 +1,9 @@
+﻿
+
+public static class DefineData
+{
+    public const int MAX_CELL_COUNT = 9;
+    public const int MAX_PACK_COUNT = 9;
+    public const int MAX_ROW_COUNT = 3;
+    public const int MAX_COLUMN_COUNT = 3;
+}
---
+++ Assets/scripts/SquareCell.cs
@@ -0,0 +1,16 @@
+﻿
+public class SquareCell
+{
+    public int OrderIndex { get { return this._orderIndex; } }
+    
+    private int _orderIndex = 0;
+    private int _value = 0;
+    private bool _isOpenValue = false;
+
+    public SquareCell(int cellIndex, int value, bool isOpenValue)
+    {
+        this._orderIndex = cellIndex;
+        this._value = _value;
+        this._isOpenValue = isOpenValue;
+    }
+}
